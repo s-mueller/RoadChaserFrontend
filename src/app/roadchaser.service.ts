c703beee5a8a412fec0faac0e512cd9206bc5c70
Sebastian Müller <sebastian.mueller@netcetera.com>
2026-06-10 12:27:04 +0200

    Deploy

--- src/app/roadchaser.service.ts
+++ src/app/roadchaser.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {CoverageDto} from './model';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoadChaserService {
 
-  public apiUrl = '/api';
+  public apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
